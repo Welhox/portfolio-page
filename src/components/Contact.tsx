ffd3e6b2e3cd259609f67c346c40bf4619d5a829
Casimir Lundberg <casimir.lundberg@gmail.com>
2025-09-04 00:46:21 +0300

    moved assets to public folder

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,13 +8,13 @@ export const Contact = () => {
           <span className="flex-1 h-px bg-slate-300"></span>
         </h2>
         <div className="flex items-center justify-center gap-8 mb-4">
-            <img src="/src/assets/icons/email.png" alt="email"
+            <img src="/icons/email.png" alt="email"
                 className="w-10 h-10 md:w-20 md:h-20 filter dark:invert hover:opacity-80 hover:scale-110" />
             <a href="mailto:casimir.lundberg@gmail.com"
                 className="text-primary2 md:text-4xl text-lg hover:scale-110 transform transition-transform inline-block">casimir.lundberg@gmail.com</a>.
         </div>
         <div className="flex items-center justify-center gap-8 mb-4">
-            <img src="/src/assets/icons/linkedin_light.png" alt="linkedin"
+            <img src="/icons/linkedin_light.png" alt="linkedin"
                 className="w-10 h-10 md:w-20 md:h-20 filter dark:invert hover:opacity-80 hover:scale-110" />
             <a href="https://linkedin.com/in/caslun" target="_blank" rel="noopener noreferrer"
                 className="text-primary2 md:text-4xl text-lg hover:scale-110 transform transition-transform inline-block">
