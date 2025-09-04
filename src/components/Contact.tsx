6d21586a8377c1149c01303cf32104f71b7be479
Casimir Lundberg <casimir.lundberg@gmail.com>
2025-09-04 14:29:37 +0300

    removed dots in end of contact rows

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -11,7 +11,7 @@ export const Contact = () => {
             <img src="/icons/email.png" alt="email"
                 className="w-10 h-10 md:w-20 md:h-20 filter dark:invert hover:opacity-80 hover:scale-110" />
             <a href="mailto:casimir.lundberg@gmail.com"
-                className="text-primary2 md:text-4xl text-lg hover:scale-110 transform transition-transform inline-block">casimir.lundberg@gmail.com</a>.
+                className="text-primary2 md:text-4xl text-lg hover:scale-110 transform transition-transform inline-block">casimir.lundberg@gmail.com</a>
         </div>
         <div className="flex items-center justify-center gap-8 mb-4">
             <img src="/icons/linkedin_light.png" alt="linkedin"
@@ -19,7 +19,7 @@ export const Contact = () => {
             <a href="https://linkedin.com/in/caslun" target="_blank" rel="noopener noreferrer"
                 className="text-primary2 md:text-4xl text-lg hover:scale-110 transform transition-transform inline-block">
                 linkedin.com/in/caslun&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            </a>.
+            </a>
         </div>
     </section>
   );
